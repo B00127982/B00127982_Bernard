6bf32bafaf153bb8d205e737a1aa03e9f96d168b
B00127982 <b00127982@mytudublin.ie>
2021-09-23 12:45:34 +0100

    added area of a circle function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 
+#define pi 3.14159265358979323846264338327950
+
 using namespace std;
+
+int getCircleArea(int a);
 
 // Base class
 class Shape
@@ -28,18 +32,33 @@ public:
    {
       return (width * height)/2;
    }
+
 };
 
 int main(void)
 {
    Triangle  Tri;
 
+   int userinput = 0;
+
    Tri.setWidth(5);
    Tri.setHeight(7);
    // Print the area of the object.
    cout << "Total Triangle area: " << Tri.getArea() << endl;
+   
+   cout << "Input the radius of the circle" << endl;
+
+   cin >> userinput;
+   
+   cout << "The area is " <<getCircleArea(userinput) << endl;
 
    system("pause");
 
    return 0;
 }
+
+int getCircleArea(int userinput)
+{
+	int radius = userinput;
+	return (pi * (radius*radius));
+};
